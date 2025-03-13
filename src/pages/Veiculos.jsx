7266c2e480bb50455258200699ab76b635d45a17
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-03-13 20:49:33 -0300

    fetch data veiculo

--- src/pages/Veiculos.jsx
+++ src/pages/Veiculos.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import Nav from "../components/Navbar";
+import api from "../services/axios";
+import Table from "../components/Table";
 
 export default function Veiculos() {
+  const [data, setData] = useState([]);
+
+  const headers = [
+    { label: "Ano", key: "ano" },
+    { label: "Marca", key: "marca" },
+    { label: "Modelo", key: "modelo" },
+    { label: "Placa", key: "placa" },
+  ];
+
+  useEffect(() => {
+    async function getData() {
+      const resposta = await api.get("/veiculo");
+      setData(resposta.data);
+    }
+    getData();
+  }, []);
   return (
     <>
       <Nav />
-      <h1>Veiculos</h1>
+
+      <div className="h-full w-full">
+        <div className="px-10 py-5">
+          <Table headers={headers} data={data} />
+        </div>
+      </div>
     </>
   );
 }
