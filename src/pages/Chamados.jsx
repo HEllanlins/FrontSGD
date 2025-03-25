91b117e4806a250bb98feb16048f70b14dba982f
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-03-25 11:45:42 -0300

    Clientes table

--- src/pages/Chamados.jsx
+++ src/pages/Chamados.jsx
@@ -6,27 +6,16 @@ export default function Chamados() {
 
   //Trocar data pela requisição pra API(ler docs do axios), e colocar em cada pagina que for usar a tabela seus respectivos headers no caso headers do chamados ja está correto
 
-  const headers = ["Cliente", "Categoria", "Observação", "Metragem", "Valor", "Status"];
+  const headers = [
+    { label: "Cliente", key: "Cliente" },
+    { label: "Categoria", key: "Categoria" },
+    { label: "Observação", key: "descricao" },
+    { label: "Metragem(m²)", key: "Metragem" },
+    { label: "Valor(R$)", key: "Valor" },
+    { label: "Status", key: "Status" },
+  ];
 
   // Passar o data com as mesmas chaves que estão no header
-  const data = [
-    {
-      Cliente: "Arthur",
-      Categoria: "Dedetização",
-      Observação: "Chegar cedo",
-      Metragem: 80,
-      Valor: 500,
-      Status: "Pendente",
-    },
-    {
-      Cliente: "Arthur",
-      Categoria: "Dedetização",
-      Observação: "Chegar cedo",
-      Metragem: 80,
-      Valor: 500,
-      Status: "Pendente",
-    },
-  ];
 
   return (
     <>
--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Nav from "../components/Navbar";
 import Table from "../components/Table";
 import api from "../services/axios";
@@ -7,16 +8,32 @@ export default function Clientes() {
 
   //Trocar data pela requisição pra API(ler docs do axios), e colocar em cada pagina que for usar a tabela seus respectivos headers no caso headers do chamados ja está correto
 
-  const headers = ["cpf_cnpj", "nome", "telefone", "endereco", "email"];
+  const [data, setData] = useState([]);
 
-  // Passar o data com as mesmas chaves que estão no header
-  const data = api.get("/cliente")
+  const headers = [
+    { label: "CPF", key: "cpf_cnpj" },
+    { label: "Nome", key: "nome" },
+    { label: "Telefone", key: "telefone" },
+    { label: "Endereço", key: "endereco" },
+    { label: "Email", key: "email" },
+  ];
 
+  useEffect(() => {
+    async function getData() {
+      const resposta = await api.get("/cliente");
+      setData(resposta.data);
+    }
+    console.log(getData());
+  }, []);
   return (
     <>
       <Nav />
-      <h1>Clientes</h1>
-      <Table headers={headers} data={data} />
+      <div className="p-5 mx-10 my-5 rounded-2xl bg-gray-300">
+        <div className="mb-10 bg-gray-200 rounded-2xl p-10">
+          <h2 className="text-3xl">Clientes</h2>
+        </div>
+        <Table headers={headers} data={data} />
+      </div>
     </>
   );
 }
