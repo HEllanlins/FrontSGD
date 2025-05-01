3a81a7c15a9e5589cff68465dc370cc2346062e9
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-05-01 14:40:59 -0300

    feat: :zap: Private route component

--- src/stores/useServiceStore.js
+++ src/stores/useServiceStore.js
@@ -2,11 +2,18 @@ import { create } from "zustand";
 
 export const useServiceStore = create((set) => ({
   services: [],
-  addService: (service) => set((state) => ({ services: [...state.services, service] })),
-  removeService: (id) => set((state) => ({ services: state.services.filter((service) => service.id !== id) })),
+  addService: (service) =>
+    set((state) => ({ services: [...state.services, service] })),
+  removeService: (id) =>
+    set((state) => ({
+      services: state.services.filter((service) => service.id !== id),
+    })),
   updateService: (id, updatedService) =>
     set((state) => ({
-      services: state.services.map((service) => (service.id === id ? { ...service, ...updatedService } : service)),
+      services: state.services.map((service) =>
+        service.id === id ? { ...service, ...updatedService } : service
+      ),
     })),
   clearServices: () => set({ services: [] }),
 }));
+//
