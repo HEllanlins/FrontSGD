25f4f618da3294ff23442406fbb53cc94571ba12
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-05-09 14:49:19 -0300

    fix: Solving a margin problem in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import Navbar from "../components/Navbar";
-import GraficoStatusServico from "../components/GraficoStatusServico";
-import GraficoMetaMensal from "../components/GraficoMetaMensal";
-import ReposicaoEstoque from "../components/ReposicaoEstoque";
-import TodoList from "../components/TodoList";
+import Navbar from '../components/Navbar';
+import GraficoStatusServico from '../components/GraficoStatusServico';
+import GraficoMetaMensal from '../components/GraficoMetaMensal';
+import ReposicaoEstoque from '../components/ReposicaoEstoque';
+import TodoList from '../components/TodoList';
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="w-full h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="w-full h-full bg-gray-100 flex flex-col items-center justify-center">
         <div className="h-1/2 w-full grid grid-cols-2 gap-4 p-4">
           <GraficoStatusServico />
           <GraficoMetaMensal />
