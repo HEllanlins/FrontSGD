68f04b857204962d6fc3488683e86739b4a98b5b
HellanLins <Hellan.lins@gmail.com>
2025-09-18 08:46:55 -0300

    Revert "alterações"
    This reverts commit 6c5bf1286dc461aed20b949aa71ef28ae7ec8fa4.

--- src/providers/Providers.jsx
+++ src/providers/Providers.jsx
@@ -3,16 +3,13 @@ import { UsuarioProvider } from "./UsuarioProvider";
 import { ProdutoProvider } from "./ProdutoProvider";
 import { ServiceProvider } from "./ServiceProvider";
 import { VeiculoProvider } from "./VeiculoProvider";
-import { AgendaProvider } from "../contexts/AgendaContext";
 
 export function Providers({ children }) {
   return (
     <UsuarioProvider>
       <ProdutoProvider>
         <ServiceProvider>
-          <VeiculoProvider>
-            <AgendaProvider>{children}</AgendaProvider>
-          </VeiculoProvider>
+          <VeiculoProvider>{children}</VeiculoProvider>
         </ServiceProvider>
       </ProdutoProvider>
     </UsuarioProvider>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -25,7 +25,6 @@ export default function AppRoutes() {
           </PrivateRoute>
         }
       />
-
       <Route
         path="/chamados"
         element={
